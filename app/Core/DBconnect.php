2a06f2e5d0aa1cce47263f671455b8e02274d656
eihents <eihents>
2024-04-10 18:40:20 +0300

    fixed

--- app/Core/DBconnect.php
+++ app/Core/DBconnect.php
@@ -4,36 +4,12 @@ class DbConnect {
     public $dbconn;
     private $config;
 
-<<<<<<< HEAD
-    function __construct() {
-        // Load database configuration
-        $this->config = require_once "../app/config.php";
-
-        // Debug: Check the content of $this->config
-        // var_dump($this->config);
-
-        // Build the DSN string for PDO
-        $dsn = "mysql:host={$this->config['host']};dbname={$this->config['dbname']};charset={$this->config['charset']}";
-
-        try {
-            // Create a PDO instance
-            $this->dbconn = new PDO($dsn, $this->config['user'], $this->config['password']);
-
-            // Set PDO attributes
-            $this->dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->dbconn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            // Handle database connection error
-            throw new Exception("Database connection error: " . $e->getMessage());
-        }
-=======
     function __construct() //connects to the db. defualt mysql
     {
         $this->config = require_once "../app/config.php";
         $this->dbconn = new PDO ('mysql:'.http_build_query($this->config,"",";"));
         $this->dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->dbconn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
->>>>>>> refs/remotes/origin/main
     }
 
     function __destruct() {
--- public/index.php
+++ public/index.php
@@ -1,26 +1,3 @@
 <?php
-<<<<<<< HEAD
-/*
-    Can add the routs and the conntroller
-*/
-$routes = [
-    '/' => '../app/Controllers/index.php',
-    '/login' => '../app/Controllers/login.php',
-    '/register' => '../app/Controllers/register.php',
-];
-
-//The router
-$url = parse_url($_SERVER['REQUEST_URI'])["path"];
-
-if (array_key_exists($url, $routes)) {
-    require $routes[$url];
-}else{
-    http_response_code(404);
-    require "../app/Controllers/404.php";
-}
-
-
-=======
 require "../app/Core/Util.php"; //for debbuging
 require "../app/router.php";
->>>>>>> refs/remotes/origin/main
