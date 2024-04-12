7946971d1f4383887a1f24875f9ece99ef853964
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-04-12 10:15:13 +0300

    Added lostPassword route

--- app/routs.php
+++ app/routs.php
@@ -8,6 +8,7 @@ return [
     '/' => '../app/Controllers/index.php',
     '/login' => '../app/Controllers/login.php',
     '/register' => '../app/Controllers/register.php',
+    '/lostPassword' => '../app/Controllers/lostPassword.php',
     '/todo' => '../app/Controllers/todo/index.php',
     '/todo/create' => '../app/Controllers/todo/index.php',
     '/todo/delete' => '../app/Controllers/todo/index.php',
