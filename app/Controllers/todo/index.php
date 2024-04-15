ea2737ff4fd9de7e0e19c3c40f4aa93028b710bc
Eihentss <ipa22.r.eihentals@vtdt.edu.lv>
2024-04-15 13:45:11 +0300

    made a session.php fille

--- app/Controllers/todo/index.php
+++ app/Controllers/todo/index.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['user'])) {
     // Check if the 'Username' key exists in the user data
     if (isset($loggedInUser['Username'])) {
         $username = $loggedInUser['Username'];
-        echo "Logged in as: $username";
+        echo "Logged in as: " . htmlspecialchars($username);
         require_once "../app/Views/todo/index.view.php";
     } else {
         echo "Username not found in session";
--- app/Core/Session.php
+++ app/Core/Session.php
@@ -1,4 +1,5 @@
 <?php
+session_set_cookie_params(3600 * 24); // save session for a day
 session_start();
 
-session_set_cookie_params(3600 * 24); // save session for a day
+
