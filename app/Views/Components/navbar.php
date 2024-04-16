6d417becfc0c6bf4e4d88fa3278845e54cbf99b0
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-04-16 13:23:03 +0300

    navbar fix

--- app/Views/Components/navbar.php
+++ app/Views/Components/navbar.php
@@ -5,8 +5,8 @@
     <div>
       <a href="/register" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">Register</a>
       <a href="/login" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">Login</a>
-      <a href="/project" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">ToDo</a>
-      <a href="/calander" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">calander</a>
+      <a href="/project" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">Projects</a>
+      <a href="/calander" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">Calander</a>
       <a href="/userSettings" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">User settings</a>
     </div>
   </nav>
