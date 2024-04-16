bdc41d23b9f81e095f60314d3a560dbd8c763ad6
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-04-16 11:02:05 +0300

    added stuff to nav bar

--- app/Views/Components/navbar.php
+++ app/Views/Components/navbar.php
@@ -5,7 +5,8 @@
     <div>
       <a href="/register" class="text-white mx-2 hover:border hover:border-white transition duration-300">Register</a>
       <a href="/login" class="text-white mx-2 hover:border hover:border-white transition duration-300">Login</a>
-      <a href="/" class="text-white mx-2 hover:border hover:border-white transition duration-300">Home</a>
+      <a href="/project" class="text-white mx-2 hover:border hover:border-white transition duration-300">Projects</a>
+      <a href="/calander" class="text-white mx-2 hover:border hover:border-white transition duration-300">Calander</a>
       <a href="/todo" class="text-white mx-2 hover:border hover:border-white transition duration-300">ToDo</a>
       <a href="/logout" class="text-white mx-2 hover:border hover:border-white transition duration-300">Logout</a>
     </div>
