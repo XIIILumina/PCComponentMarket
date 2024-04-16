9184def3d46663341cced4192950e9a79df37e6e
Eihentss <ipa22.r.eihentals@vtdt.edu.lv>
2024-04-16 13:36:53 +0300

    nav

--- app/Views/Components/navbar.php
+++ app/Views/Components/navbar.php
@@ -1,12 +1,12 @@
-  <nav class="flex justify-between bg-gradient-to-r from-blue-300 from-10% via-blue-100 via-30% to-blue-300 to-90%  p-3">
+<nav class="flex justify-between bg-gradient-to-r from-blue-300 from-10% via-blue-100 via-30% to-blue-300 to-90%  p-3">
     <div>
     <a href="/"><img src="../../public/images/fency2.png" alt="Logo" class=" bg-gradient-to-r from-blue-500 from-20% via-gray-400 via-30% to-sky-200 to-90%  rounded-l px-1 transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110 h-12 w-auto inline block "></a>
     </div>
     <div>
       <a href="/register" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">Register</a>
       <a href="/login" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">Login</a>
-      <a href="/project" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">Project</a>
-      <a href="/calander" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">calander</a>
+      <a href="/project" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">Projects</a>
+      <a href="/calander" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">Calander</a>
       <a href="/userSettings" class="text-black mx-2 btn inline-block bg-blue-100 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:bg-blue-500 hover:scale-110">User settings</a>
     </div>
   </nav>
