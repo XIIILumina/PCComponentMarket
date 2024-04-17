8097ee600167867a13ebaef8f74ef2ba423eaf5c
XIIILumina <ipa22.a.zibrevs@vtdt.edu.lv>
2024-04-17 13:42:45 +0300

    black men

--- app/Views/user/userSettings.view.php
+++ app/Views/user/userSettings.view.php
@@ -1,16 +1,16 @@
 <?php require_once "../app/Views/Components/head.php"; ?>
 <?php require_once "../app/Views/Components/navbar.php"; ?>
-<div class="h-screen">
+<div class="h-screen bg-white shadow-xl h-6 w-auto grid content-right text-white py-1 px-1 hover:shadow-3xl transition duration-500">
 
-    <a href="/logout"><button class="text-blue-500 border border-blue-500 rounded">Logout</button></a>
+    <a href="/logout"><button class="bg-blue-500 p-1 border border-blue-500 rounded-lg">Logout</button></a>
     <from method="POST">
-        <button class="text-blue-500 border border-blue-500 rounded">Change Password</button>
+        <button class="bg-blue-500 text-white p-1 border border-blue-500 rounded-lg">Change Password</button>
     </from>
     <from method="POST">
-        <button class="text-blue-500 border border-blue-500 rounded">Change Email</button>
+        <button class="bg-blue-500 text-white p-1 border border-blue-500 rounded-lg">Change Email</button>
     </from>
     <from method="POST">
-        <button class="text-red-500 border border-red-500 rounded">Delete Account</button>
+        <button class="bg-red-500 text-white p-1 border-red-500 rounded">Delete Account</button>
     </from>
 
 </div>
