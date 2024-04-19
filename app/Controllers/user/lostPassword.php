ae7bf0efc38cd41d76776184834605d869518e16
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-04-19 08:55:14 +0300

    added a view to lostPassword

---
+++ app/Controllers/user/lostPassword.php
@@ -0,0 +1,8 @@
+<?php
+require "../app/Core/Session.php";
+if (isset($_SESSION['user']['Username'])) {
+    header("Location: /login");
+}
+
+$title = "lost Password";
+require "../app/Views/user/lostPassword.view.php";
