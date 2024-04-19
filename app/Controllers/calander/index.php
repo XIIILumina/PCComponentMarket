ff171998037fd55bf7f9d13d96502c18fdf44bee
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-04-19 08:47:25 +0300

    only can access user settings when loged in

--- app/Controllers/calander/index.php
+++ app/Controllers/calander/index.php
@@ -1,3 +1,8 @@
 <?php
+require "../app/Core/Session.php";
+if (!isset($_SESSION['user']['Username'])) {
+    header("Location: /login");
+}
+
 $title = "Calander";
 require_once "../app/Views/calander/index.view.php";
--- app/Controllers/user/userSettings.php
+++ app/Controllers/user/userSettings.php
@@ -1,6 +1,11 @@
 <?php
 require "../app/Models/user.php";
 require "../app/Core/Validator.php";
+require "../app/Core/Session.php";
+
+if (!isset($_SESSION['user']['Username'])) {
+    header("Location: /login");
+}
 $userModel = new userModel();
 
 $title = "Register";
